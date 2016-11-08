273caf0d69ff42e6198a61481598b77c7c3a96b5
Your Name <arssonist@yahoo.com>
2016-11-08 14:12:12 -0500

    added functionality to 3 buttons, returning html and csv so far

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,19 @@
 class BooksController < ApplicationController
 
+  def index
+    @books = Book.all
+
+    respond_to do |format|
+      format.html
+      format.text
+
+      format.csv do
+         render plain: Book.generate_csv(@books)
+      end
+
+    end
+
+
+  end
 
 end
